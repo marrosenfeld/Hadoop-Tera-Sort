2159118ecd6f71658b71960853eff3c95d5be0d3
Martin Rosenfeld <mrosenf4@hawk.iit.edu>
2016-03-17 11:11:35 -0500

    update scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,13 +4,29 @@ filelines=`cat $filename`
 echo Start
 for p in $filelines ; do
   	echo $p:
-	#scp -i prod.pem prod.pem ubuntu@$p:
-	#scp -i prod.pem ssh_script.sh ubuntu@$p:
-	#ssh -i prod.pem ubuntu@$p 'sudo chmod +x ssh_script.sh'
-	#ssh -i prod.pem ubuntu@$p './ssh_script.sh'
-	#scp -i prod.pem filesystem.sh ubuntu@$p:
+	scp -i prod.pem prod.pem ubuntu@$p:
+	scp -i prod.pem ssh_script.sh ubuntu@$p:
+	ssh -i prod.pem ubuntu@$p 'sudo chmod +x ssh_script.sh'
+	ssh -i prod.pem ubuntu@$p './ssh_script.sh'
+	scp -i prod.pem filesystem.sh ubuntu@$p:
 	
-	#ssh -i prod.pem ubuntu@$p './filesystem.sh'
+	ssh -i prod.pem ubuntu@$p './filesystem.sh'
 	scp -i prod.pem confFiles/* ubuntu@$p:/home/ubuntu/hadoop/etc/hadoop/
-	echo 'adsa'
 done
+
+filename='confFiles/masters'
+filelines=`cat $filename`
+echo Start
+for p in $filelines ; do
+        echo $p:
+        scp -i prod.pem prod.pem ubuntu@$p:
+	scp -i prod.pem ssh_script.sh ubuntu@$p:
+	ssh -i prod.pem ubuntu@$p 'sudo chmod +x ssh_script.sh'
+	ssh -i prod.pem ubuntu@$p './ssh_script.sh'
+	scp -i prod.pem filesystem.sh ubuntu@$p:
+        ssh -i prod.pem ubuntu@$p './filesystem.sh'
+        scp -i prod.pem confFiles/* ubuntu@$p:/home/ubuntu/hadoop/etc/hadoop/
+	scp -i prod.pem start.sh ubuntu@$p:
+	scp -i prod.pem run.sh ubuntu@$p:
+  	scp -i prod.pem target/hadoop-tera-sort-0.0.1-SNAPSHOT.jar ubuntu@$p:
+done 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 hadoop/bin/hdfs namenode -format
 hadoop/sbin/start-dfs.sh
+hadoop/sbin/start-yarn.sh
